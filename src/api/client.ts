b57943ce7ae05282058c0e90b8d1b25094917a3e
Eugene Shalaev <anarxict97@mail.ru>
2026-02-04 22:35:51 +1000

    feat: добавил env файлы

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import type { Item, PaginatedResponse } from "../types";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export class ApiClient {
   private static async request<T>(
